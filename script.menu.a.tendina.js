33f3e4e843a36f233663d73b0a45902da8612927
mauriziosfilio <63847429+mauriziosfilio@users.noreply.github.com>
2020-04-17 11:54:28 +0200

    Add files via upload

---
+++ script.menu.a.tendina.js
@@ -0,0 +1,11 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+//per inciso non so cosa sto facendo qui
+//ad ogni click sul menu a 3 barre il toggle cambia e cambia classe consentendoci di animare la croce
+//come segue
+//anzitutto torniamo in css e definiamo una nuova classe per effettuare un cambio di colore
+//alle barre quando diventano una x
+
